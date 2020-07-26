4f53eab4a444105a337f00da0bd9ca5fbf66ca93
LiamMZ <lime4992@colorado.edu>
2020-07-25 20:44:22 -0400

    fixed files

--- src/object_detection.cpp
+++ src/object_detection.cpp
@@ -1,12 +1,14 @@
 
-#include <Mobile-Robot-Project/object_detector.h>
+#include "object_detector.h"
 #include <fstream>
 #include <sstream>
 
 #include <opencv2/dnn.hpp>
+#include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
+#include <algorithm>
 #include <mutex>
 #include <thread>
 #include <queue>
@@ -14,32 +16,37 @@
 using namespace cv;
 using namespace dnn;
 
-ObjectDetection::ObjectDetection(ros::nodeHandle nh, std::string model_file,
-                   std::string config_file, std::string classes_file) : nh_(nh)
+ObjectDetector::ObjectDetector(ros::NodeHandle nh, std::string model_file,
+                   std::string config_file, std::string classes_file, bool show_image) : nh_(nh)
 {
     std::ifstream ifs(classes_file.c_str());
     if (!ifs.is_open())
-        CV_Error(Error::StsError, "File " + file + " not found");
+        CV_Error(Error::StsError, "File " + classes_file + " not found");
     std::string line;
     while (std::getline(ifs, line))
     {
         classes_.push_back(line);
     }
 
-    Net net_ = readNet(modelPath, configPath, parser.get<String>("framework"));
+    net_ = readNet(model_file, config_file);
+    scale_ = 0.00392;
+    confThreshold_ = 0.5;
+    outNames_ = net_.getUnconnectedOutLayersNames();
+    show_image_ = show_image;
+
 }
 
-inline void ObjectDetection::preprocess(const Mat& frame, Net& net_, Size inpSize, float scale,
+void ObjectDetector::preprocess(const Mat& frame, Size inpSize,
                        const Scalar& mean, bool swapRB)
 {
     static Mat blob;
     // Create a 4D blob from a frame.
     if (inpSize.width <= 0) inpSize.width = frame.cols;
     if (inpSize.height <= 0) inpSize.height = frame.rows;
-    blobFromImage(frame, blob, 1.0, inpSize, Scalar(), swapRB, false, CV_8U);
+    blobFromImage(frame, blob, 1, cv::Size(416, 416));
 
     // Run a model.
-    net_.setInput(blob, "", scale, mean);
+    net_.setInput(blob, "", scale_, mean);
     if (net_.getLayer(0)->outputNameToIndex("im_info") != -1)  // Faster-RCNN or R-FCN
     {
         resize(frame, frame, inpSize);
@@ -48,7 +55,7 @@ inline void ObjectDetection::preprocess(const Mat& frame, Net& net_, Size inpSiz
     }
 }
 
-void ObjectDetection::postprocess(Mat& frame, const std::vector<Mat>& outs, Net& net_, int backend)
+void ObjectDetector::postprocess(Mat& frame, const std::vector<Mat>& outs)
 {
     static std::vector<int> outLayers = net_.getUnconnectedOutLayers();
     static std::string outLayerType = net_.getLayer(outLayers[0])->type;
@@ -56,43 +63,43 @@ void ObjectDetection::postprocess(Mat& frame, const std::vector<Mat>& outs, Net&
     std::vector<int> classIds;
     std::vector<float> confidences;
     std::vector<Rect> boxes;
-    if (outLayerType == "DetectionOutput")
-    {
-        // Network produces output blob with a shape 1x1xNx7 where N is a number of
-        // detections and an every detection is a vector of values
-        // [batchId, classId, confidence, left, top, right, bottom]
-        CV_Assert(outs.size() > 0);
-        for (size_t k = 0; k < outs.size(); k++)
-        {
-            float* data = (float*)outs[k].data;
-            for (size_t i = 0; i < outs[k].total(); i += 7)
-            {
-                float confidence = data[i + 2];
-                if (confidence > confThreshold)
-                {
-                    int left   = (int)data[i + 3];
-                    int top    = (int)data[i + 4];
-                    int right  = (int)data[i + 5];
-                    int bottom = (int)data[i + 6];
-                    int width  = right - left + 1;
-                    int height = bottom - top + 1;
-                    if (width <= 2 || height <= 2)
-                    {
-                        left   = (int)(data[i + 3] * frame.cols);
-                        top    = (int)(data[i + 4] * frame.rows);
-                        right  = (int)(data[i + 5] * frame.cols);
-                        bottom = (int)(data[i + 6] * frame.rows);
-                        width  = right - left + 1;
-                        height = bottom - top + 1;
-                    }
-                    classIds.push_back((int)(data[i + 1]) - 1);  // Skip 0th background class id.
-                    boxes.push_back(Rect(left, top, width, height));
-                    confidences.push_back(confidence);
-                }
-            }
-        }
-    }
-    else if (outLayerType == "Region")
+    // if (outLayerType == "DetectionOutput")
+    // {
+    //     // Network produces output blob with a shape 1x1xNx7 where N is a number of
+    //     // detections and an every detection is a vector of values
+    //     // [batchId, classId, confidence, left, top, right, bottom]
+    //     CV_Assert(outs.size() > 0);
+    //     for (size_t k = 0; k < outs.size(); k++)
+    //     {
+    //         float* data = (float*)outs[k].data;
+    //         for (size_t i = 0; i < outs[k].total(); i += 7)
+    //         {
+    //             float confidence = data[i + 2];
+    //             if (confidence > confThreshold_)
+    //             {
+    //                 int left   = (int)data[i + 3];
+    //                 int top    = (int)data[i + 4];
+    //                 int right  = (int)data[i + 5];
+    //                 int bottom = (int)data[i + 6];
+    //                 int width  = right - left + 1;
+    //                 int height = bottom - top + 1;
+    //                 if (width <= 2 || height <= 2)
+    //                 {
+    //                     left   = (int)(data[i + 3] * frame.cols);
+    //                     top    = (int)(data[i + 4] * frame.rows);
+    //                     right  = (int)(data[i + 5] * frame.cols);
+    //                     bottom = (int)(data[i + 6] * frame.rows);
+    //                     width  = right - left + 1;
+    //                     height = bottom - top + 1;
+    //                 }
+    //                 classIds.push_back((int)(data[i + 1]) - 1);  // Skip 0th background class id.
+    //                 boxes.push_back(Rect(left, top, width, height));
+    //                 confidences.push_back(confidence);
+    //             }
+    //         }
+    //     }
+    // }
+    if (outLayerType == "Region")
     {
         for (size_t i = 0; i < outs.size(); ++i)
         {
@@ -106,7 +113,7 @@ void ObjectDetection::postprocess(Mat& frame, const std::vector<Mat>& outs, Net&
                 Point classIdPoint;
                 double confidence;
                 minMaxLoc(scores, 0, &confidence, 0, &classIdPoint);
-                if (confidence > confThreshold)
+                if (confidence > confThreshold_ && std::find(classIds.begin(),classIds.end(), classIdPoint.x)==classIds.end())
                 {
                     int centerX = (int)(data[0] * frame.cols);
                     int centerY = (int)(data[1] * frame.rows);
@@ -127,43 +134,43 @@ void ObjectDetection::postprocess(Mat& frame, const std::vector<Mat>& outs, Net&
 
     // NMS is used inside Region layer only on DNN_BACKEND_OPENCV for another backends we need NMS in sample
     // or NMS is required if number of outputs > 1
-    if (outLayers.size() > 1 || (outLayerType == "Region" && backend != DNN_BACKEND_OPENCV))
-    {
-        std::map<int, std::vector<size_t> > class2indices;
-        for (size_t i = 0; i < classIds.size(); i++)
-        {
-            if (confidences[i] >= confThreshold)
-            {
-                class2indices[classIds[i]].push_back(i);
-            }
-        }
-        std::vector<Rect> nmsBoxes;
-        std::vector<float> nmsConfidences;
-        std::vector<int> nmsClassIds;
-        for (std::map<int, std::vector<size_t> >::iterator it = class2indices.begin(); it != class2indices.end(); ++it)
-        {
-            std::vector<Rect> localBoxes;
-            std::vector<float> localConfidences;
-            std::vector<size_t> classIndices = it->second;
-            for (size_t i = 0; i < classIndices.size(); i++)
-            {
-                localBoxes.push_back(boxes[classIndices[i]]);
-                localConfidences.push_back(confidences[classIndices[i]]);
-            }
-            std::vector<int> nmsIndices;
-            NMSBoxes(localBoxes, localConfidences, confThreshold, nmsThreshold, nmsIndices);
-            for (size_t i = 0; i < nmsIndices.size(); i++)
-            {
-                size_t idx = nmsIndices[i];
-                nmsBoxes.push_back(localBoxes[idx]);
-                nmsConfidences.push_back(localConfidences[idx]);
-                nmsClassIds.push_back(it->first);
-            }
-        }
-        boxes = nmsBoxes;
-        classIds = nmsClassIds;
-        confidences = nmsConfidences;
-    }
+    // if (outLayers.size() > 1 || (outLayerType == "Region" && backend != DNN_BACKEND_OPENCV))
+    // {
+    //     std::map<int, std::vector<size_t> > class2indices;
+    //     for (size_t i = 0; i < classIds.size(); i++)
+    //     {
+    //         if (confidences[i] >= confThreshold)
+    //         {
+    //             class2indices[classIds[i]].push_back(i);
+    //         }
+    //     }
+    //     std::vector<Rect> nmsBoxes;
+    //     std::vector<float> nmsConfidences;
+    //     std::vector<int> nmsClassIds;
+    //     for (std::map<int, std::vector<size_t> >::iterator it = class2indices.begin(); it != class2indices.end(); ++it)
+    //     {
+    //         std::vector<Rect> localBoxes;
+    //         std::vector<float> localConfidences;
+    //         std::vector<size_t> classIndices = it->second;
+    //         for (size_t i = 0; i < classIndices.size(); i++)
+    //         {
+    //             localBoxes.push_back(boxes[classIndices[i]]);
+    //             localConfidences.push_back(confidences[classIndices[i]]);
+    //         }
+    //         std::vector<int> nmsIndices;
+    //         NMSBoxes(localBoxes, localConfidences, confThreshold, nmsThreshold, nmsIndices);
+    //         for (size_t i = 0; i < nmsIndices.size(); i++)
+    //         {
+    //             size_t idx = nmsIndices[i];
+    //             nmsBoxes.push_back(localBoxes[idx]);
+    //             nmsConfidences.push_back(localConfidences[idx]);
+    //             nmsClassIds.push_back(it->first);
+    //         }
+    //     }
+    //     boxes = nmsBoxes;
+    //     classIds = nmsClassIds;
+    //     confidences = nmsConfidences;
+    // }
 
     for (size_t idx = 0; idx < boxes.size(); ++idx)
     {
@@ -173,7 +180,7 @@ void ObjectDetection::postprocess(Mat& frame, const std::vector<Mat>& outs, Net&
     }
 }
 
-void ObjectDetection::drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame)
+void ObjectDetector::drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame)
 {
     rectangle(frame, Point(left, top), Point(right, bottom), Scalar(0, 255, 0));
 
@@ -191,4 +198,21 @@ void ObjectDetection::drawPred(int classId, float conf, int left, int top, int r
     rectangle(frame, Point(left, top - labelSize.height),
               Point(left + labelSize.width, top + baseLine), Scalar::all(255), FILLED);
     putText(frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.5, Scalar());
+}
+
+
+void ObjectDetector::run(Mat &image)
+{
+    int inpWidth = image.cols;
+    int inpHeight = image.rows;
+    preprocess(image, Size(image.cols, image.rows), cv::mean(image), false);
+    std::vector<Mat> outs;
+    net_.forward(outs, outNames_);
+    postprocess(image, outs);
+    if (show_image_){
+        namedWindow( "Display window" );// Create a window for display.
+        imshow( "Display window", image );                   // Show our image inside it.
+
+        waitKey(0);
+    }
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,1 +1,59 @@
-#include <mobile_robot_project/object_detection.h>
+#include "object_detector.h"
+#include <sensor_msgs/CompressedImage.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include<opencv2/core/core.hpp>
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+
+void callback(const sensor_msgs::CompressedImage image_msg)
+{
+
+}
+
+
+class ImageGrabber
+{
+public:
+    void GrabImage(const sensor_msgs::ImageConstPtr& msg);
+    cv::Mat image;
+};
+
+void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
+{
+    // Copy the ros image message to cv::Mat.
+    cv_bridge::CvImageConstPtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvShare(msg);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+    this->image = cv_ptr->image;
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "talker");
+    ros::NodeHandle nodeHandler;
+    ImageGrabber igb();
+
+    ros::NodeHandle nodeHandler;
+    image_transport::ImageTransport it_(nodeHandler);
+    image_transport::Subscriber sub = it_.subscribe("/phone1/camera/image", 1, &ImageGrabber::GrabImage,&igb);
+    // cv::Mat image = cv::imread("/home/liam/catkin_ws/src/mobile_robot_project/src/man.jpg");
+    // namedWindow( "Display window1" );// Create a window for display.
+    // imshow( "Display window1", image );                   // Show our image inside it.
+
+    //     waitKey(0);
+    ObjectDetector object_detector = ObjectDetector(nodeHandler, "/home/liam/catkin_ws/src/mobile_robot_project/src/yolov3.weights",
+                                                            "/home/liam/catkin_ws/src/mobile_robot_project/src/yolov3.cfg", "/home/liam/catkin_ws/src/mobile_robot_project/src/yolov3.txt", true);
+    while(ros::ok()){
+        object_detector.run(igb.image);
+    }
+    
+
+}
